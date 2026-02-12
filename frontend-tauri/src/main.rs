704533944c759cee3489fe30ad1d267b1450ef16
Michael Morrissey <mike.d.morrissey@gmail.com>
2026-02-12 14:06:45 -0500

    feat(frontend-tauri): add tray/menu-bar background app skeleton
    enable tauri tray-icon feature and wire tray menu actions
    
    support open/hide/quit and close-to-tray window behavior

--- frontend-tauri/src/main.rs
+++ frontend-tauri/src/main.rs
@@ -1,16 +1,80 @@
+use tauri::Manager;
+use tauri::menu::{Menu, MenuItem};
+use tauri::tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent};
+
 fn build_tauri_builder() -> tauri::Builder<tauri::Wry> {
-    tauri::Builder::default().invoke_handler(tauri::generate_handler![
-        frontend_tauri::tauri_commands::app_health_command,
-        frontend_tauri::tauri_commands::system_capability_command,
-        frontend_tauri::tauri_commands::validate_model_path_command,
-        frontend_tauri::tauri_commands::validate_audio_path_command,
-        frontend_tauri::tauri_commands::pick_model_path_command,
-        frontend_tauri::tauri_commands::pick_audio_path_command,
-        frontend_tauri::tauri_commands::run_transcription_command,
-        frontend_tauri::tauri_commands::run_transcription_with_options_command,
-        frontend_tauri::tauri_commands::export_transcript_command,
-        frontend_tauri::tauri_commands::open_output_folder_command,
-    ])
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            frontend_tauri::tauri_commands::app_health_command,
+            frontend_tauri::tauri_commands::system_capability_command,
+            frontend_tauri::tauri_commands::validate_model_path_command,
+            frontend_tauri::tauri_commands::validate_audio_path_command,
+            frontend_tauri::tauri_commands::pick_model_path_command,
+            frontend_tauri::tauri_commands::pick_audio_path_command,
+            frontend_tauri::tauri_commands::run_transcription_command,
+            frontend_tauri::tauri_commands::run_transcription_with_options_command,
+            frontend_tauri::tauri_commands::export_transcript_command,
+            frontend_tauri::tauri_commands::open_output_folder_command,
+        ])
+        .setup(|app| {
+            let open_item = MenuItem::with_id(app, "open", "Open App", true, None::<&str>)?;
+            let hide_item = MenuItem::with_id(app, "hide", "Hide App", true, None::<&str>)?;
+            let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+            let tray_menu = Menu::with_items(app, &[&open_item, &hide_item, &quit_item])?;
+
+            TrayIconBuilder::new()
+                .menu(&tray_menu)
+                .on_menu_event(|app, event| match event.id().as_ref() {
+                    "open" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.unminimize();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    "hide" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.hide();
+                        }
+                    }
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            match window.is_visible() {
+                                Ok(true) => {
+                                    let _ = window.hide();
+                                }
+                                Ok(false) | Err(_) => {
+                                    let _ = window.show();
+                                    let _ = window.unminimize();
+                                    let _ = window.set_focus();
+                                }
+                            }
+                        }
+                    }
+                })
+                .build(app)?;
+
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                api.prevent_close();
+                let _ = window.hide();
+            }
+        })
 }
 
 fn main() {
